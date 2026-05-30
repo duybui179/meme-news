cfb9e236af9dbb7dbac8dde9f89837084f5eed1b
duybui179 <107290842+duybui179@users.noreply.github.com>
2026-05-30 10:36:55 +0000

    update refactor

--- internal/api/getContent.go
+++ internal/api/getContent.go
@@ -2,9 +2,35 @@ package api
 
 import(
 	"fmt"
+	"dev.duybt/internal/api"
 )
 
 func GetContent(){
 	result := "meme"
 	fmt.Println("Check content", result)
+
+	api.GetContent()
+	
+	url := "https://jsonplaceholder.typicode.com/photos"
+
+	photos, err := api.GetPhotoByURL(url)
+
+	if err != nil {
+        panic(err)
+    }
+
+	 for i := 0; i < 5; i++ {
+        fmt.Printf("%d: %s (%s)\n", photos[i].ID, photos[i].Title, photos[i].URL)
+    }
+
+	 urls := []string{
+        "https://jsonplaceholder.typicode.com/photos",
+        "https://jsonplaceholder.typicode.com/comments", // ví dụ thêm
+    }
+
+	results := api.GetContenGoroutine(urls)
+
+	for i, photos := range results {
+        fmt.Printf("URL %d có %d phần tử\n", i+1, len(photos))
+    }
 }
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"dev.duybt/internal/api"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,38 +10,14 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	api.GetContent()
+
+	http.HandleFunc("/", helloHandler)
+
+	port := ":8081"
+	fmt.Printf("Server đang khởi động tại http://localhost%s\n", port)
 	
-	url := "https://jsonplaceholder.typicode.com/photos"
-
-	photos, err := api.GetPhotoByURL(url)
-
-	if err != nil {
-        panic(err)
-    }
-
-	 for i := 0; i < 5; i++ {
-        fmt.Printf("%d: %s (%s)\n", photos[i].ID, photos[i].Title, photos[i].URL)
-    }
-
-	 urls := []string{
-        "https://jsonplaceholder.typicode.com/photos",
-        "https://jsonplaceholder.typicode.com/comments", // ví dụ thêm
-    }
-
-	results := api.GetContenGoroutine(urls)
-
-	for i, photos := range results {
-        fmt.Printf("URL %d có %d phần tử\n", i+1, len(photos))
-    }
-	
-	// http.HandleFunc("/", helloHandler)
-
-	// port := ":8081"
-	// fmt.Printf("Server đang khởi động tại http://localhost%s\n", port)
-	
-	// // Bắt đầu lắng nghe và phục vụ
-	// if err := http.ListenAndServe(port, nil); err != nil {
-	// 	fmt.Printf("Lỗi khởi động server: %s\n", err)
-	// }
+	// Bắt đầu lắng nghe và phục vụ
+	if err := http.ListenAndServe(port, nil); err != nil {
+		fmt.Printf("Lỗi khởi động server: %s\n", err)
+	}
 }
